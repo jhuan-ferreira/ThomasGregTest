4ed1e0fffca0d63953774ce55061b209900c00c5
Jhuan Ferreira Silva <ruan.2604@outlook.com>
2024-03-29 17:17:25 -0300

    Initial commit. ThomasGreg Teste

---
+++ ThomasGregTest.Data/Script DB/Script Table - Logradouro - ThomasGregTest.sql
@@ -0,0 +1,21 @@
+
+GO
+
+/****** Object:  Table [dbo].[Logradouro]    Script Date: 29/03/2024 17:00:04 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Logradouro](
+	[LogradouroId] [int] IDENTITY(1,1) NOT NULL,
+	[LogradouroNome] [varchar](250) NOT NULL,
+	[ClienteId] [int] NOT NULL,
+ CONSTRAINT [PK_Logradouro] PRIMARY KEY CLUSTERED 
+(
+	[LogradouroId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
--- ThomasGregTest.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThomasGregTest.UI.MvcApplication" Language="C#" %>
